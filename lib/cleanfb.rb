7d0d346a1d37fa16853973aae6937da0b5883585
root <root@foreman.vai.org>
2016-07-12 16:21:54 -0400

    i

--- lib/cleanfb.rb
+++ lib/cleanfb.rb
@@ -45,7 +45,8 @@ module Cleanfb
 
   	     		path += "/" + start[0..15] + sum + "/"
 	  	      puts "Removing " + path 
-  	 		    cmd = `rm  #{path}path #{path}contents`
+  	 		    cmd = `rm  #{path}paths`
+					 	cmd = `rm	#{path}contents`
 		 	    end
   	  	else
 	    	  puts "No file #{arg} found."
