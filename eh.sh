49b815b8af748e943e05434d3a7d54e00167e303
root <root@foreman.vai.org>
2016-07-19 14:26:30 -0400

    i

--- eh.sh
+++ eh.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+#!/usr/bin/expect -f
 
 git add .
 git commit -m 'i'
-git push -u origin master
-echo lhoffl
-echo ${args[0]}
+spawn git push -u origin master
+
+expect "Username for 'https://github.com': "
+send "lhoffl\r"
+expect "Password for 'https://lhoffl@github.com': "
+send "${args[0]}\r"
 
 bundle exec rake release 
-echo lhoffl
-echo ${args[0]}
-echo lhoffl
-echo ${args[0]}
+expect "Username for 'https://github.com': "
+send "lhoffl\r"
+expect "Password for 'https://lhoffl@github.com': "
+send "${args[0]}\r"
